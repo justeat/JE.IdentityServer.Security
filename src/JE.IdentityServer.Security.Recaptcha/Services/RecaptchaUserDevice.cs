5c68b250136701475f28a38467594844050f469b
Adam Morrison <adam.morrison@just-eat.com>
2019-07-29 18:27:46 +0100

    Remove spacing between props

--- src/JE.IdentityServer.Security.Recaptcha/Services/RecaptchaUserDevice.cs
+++ src/JE.IdentityServer.Security.Recaptcha/Services/RecaptchaUserDevice.cs
@@ -3,11 +3,8 @@ namespace JE.IdentityServer.Security.Recaptcha.Services
     public class RecaptchaUserDevice
     {
         public string Id { get; set;}
-
         public string Type { get; set;}
-
         public string Name { get; set;}
-
         public string Token { get; set; }
     }
 }
